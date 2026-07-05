2c35752734252c9cc1dc042410c2d0f5b6b55f1e
kmiya <kmiya@outlook.com>
2026-07-05 23:37:45 +0900

    Add my solution for assignment 6-1

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <netdb.h>
+#include <pthread.h>
 #include <signal.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -11,6 +12,9 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <syslog.h>
+#include <time.h>
+
+#include "queue.h"
 
 enum { BUFFER_SIZE = 1024 };
 
@@ -82,21 +86,11 @@ int create_socket_and_bind() {
   return socket_fd;
 }
 
-int receive_and_write(const int client_fd, FILE* write_file) {
-  char buffer[BUFFER_SIZE];
-  ssize_t received_size = 0;
-  while ((received_size = recv(client_fd, buffer, BUFFER_SIZE - 1, 0)) > 0) {
-    buffer[received_size] = '\0';
-
-    const size_t written = fwrite(buffer, 1, received_size, write_file);
-    if (written != (size_t)received_size) {
-      syslog(LOG_ERR, "Could not write file");
-      return -1;
-    }
-
-    if (memchr(buffer, '\n', received_size) != NULL) {
-      break;
-    }
+int write_log_file(char* buffer, size_t size, FILE* write_file) {
+  const size_t written = fwrite(buffer, 1, size, write_file);
+  if (written != size) {
+    syslog(LOG_ERR, "Could not write file");
+    return -1;
   }
   return 0;
 }
@@ -139,6 +133,94 @@ bool parse_daemon_arg(const int argc, char** argv) {
   return false;
 }
 
+typedef struct {
+  FILE* write_file;
+  pthread_mutex_t* mutex;
+  int client_fd;
+  struct sockaddr_in client_addr;
+  bool completed;
+} thread_args;
+
+void* timer_thread_func(void* timer_thread_args) {
+  thread_args* args = timer_thread_args;
+  struct tm tm_buf;
+
+  int elapsed_ms = 0;
+  const int check_interval_ms = 100;        // Check signal every 100ms
+  const int timestamp_interval_ms = 10000;  // Write timestamp every 10s
+
+  while (!caught_signal) {
+    const struct timespec interval = {.tv_sec = 0,
+                                      .tv_nsec = check_interval_ms * 1000000L};
+    nanosleep(&interval, NULL);
+    elapsed_ms += check_interval_ms;
+
+    if (elapsed_ms < timestamp_interval_ms) {
+      continue;
+    }
+    elapsed_ms = 0;
+
+    const time_t now = time(NULL);
+    if (localtime_r(&now, &tm_buf) != NULL) {
+      char buf[128];
+      strcpy(buf, "timestamp:");
+      strftime(buf + strlen(buf), sizeof(buf) - strlen(buf),
+               "%a, %d %b %Y %T %z\n", &tm_buf);
+      pthread_mutex_lock(args->mutex);
+      if (write_log_file(buf, (ssize_t)strlen(buf), args->write_file) == -1) {
+        syslog(LOG_ERR, "Failed to write timestamp");
+      }
+      pthread_mutex_unlock(args->mutex);
+    }
+  }
+  return NULL;
+}
+
+void* connection_thread_func(void* con_thread_args) {
+  thread_args* args = con_thread_args;
+
+  char buffer[BUFFER_SIZE];
+  ssize_t received_size = 0;
+  pthread_mutex_lock(args->mutex);
+  while ((received_size = recv(args->client_fd, buffer, BUFFER_SIZE - 1, 0)) >
+         0) {
+    buffer[received_size] = '\0';
+
+    if (write_log_file(buffer, received_size, args->write_file) == -1) {
+      syslog(LOG_ERR, "Failed to write received data to file");
+      pthread_mutex_unlock(args->mutex);
+      args->completed = true;
+      return NULL;
+    }
+
+    if (memchr(buffer, '\n', received_size) != NULL) {
+      break;
+    }
+  }
+
+  if (fseek(args->write_file, 0, SEEK_SET) != 0) {
+    syslog(LOG_ERR, "fseek failed");
+  }
+
+  // send -----------------------------------------------
+  if (send_file(args->client_fd, args->write_file) == -1) {
+    syslog(LOG_ERR, "Failed to send back received data");
+    pthread_mutex_unlock(args->mutex);
+    args->completed = true;
+    return NULL;
+  }
+  pthread_mutex_unlock(args->mutex);
+  args->completed = true;
+  return NULL;
+}
+
+typedef struct th_node_s th_node_t;
+struct th_node_s {
+  thread_args* data;
+  pthread_t thread_id;
+  SLIST_ENTRY(th_node_s) next;
+};
+
 int main(int argc, char** argv) {
   openlog("aesdsocket", LOG_CONS | LOG_PERROR, LOG_USER);
 
@@ -180,6 +262,27 @@ int main(int argc, char** argv) {
     exit(EXIT_FAILURE);  // NOLINT(concurrency-mt-unsafe)
   }
 
+  // mutex for write file
+  static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+  // Start timer thread -----------------------------------
+  pthread_t timer_thread = 0;
+  thread_args* timer_args = malloc(sizeof(thread_args));
+  timer_args->write_file = write_file;
+  timer_args->mutex = &mutex;
+  int timer_result =
+      pthread_create(&timer_thread, NULL, timer_thread_func, (void*)timer_args);
+  if (timer_result) {
+    syslog(LOG_ERR, "Cannot create timer thread");
+    free(timer_args);
+    close(socket_fd);
+    closelog();
+    exit(EXIT_FAILURE);  // NOLINT(concurrency-mt-unsafe)
+  }
+
+  SLIST_HEAD(thread_list, th_node_s) head;
+  SLIST_INIT(&head);
+
   // Main loop --------------------------------------------
   while (!caught_signal) {
     // Accept ---------------------------------------------
@@ -193,44 +296,71 @@ int main(int argc, char** argv) {
         continue;
       }
       syslog(LOG_ERR, "Could not accept");
-      close(socket_fd);
-      closelog();
-      exit(EXIT_FAILURE);  // NOLINT(concurrency-mt-unsafe)
+      continue;
     }
     syslog(LOG_INFO, "Accepted connection from %s",
            inet_ntoa(client_addr.sin_addr));  // NOLINT(concurrency-mt-unsafe)
 
-    // recv ---------------------------------------------
-    if (receive_and_write(client_fd, write_file) == -1) {
-      close(socket_fd);
-      closelog();
-      return 1;
+    // Create a thread for each connection -----------------
+    pthread_t con_thread = 0;
+    thread_args* con_args = malloc(sizeof(thread_args));
+    con_args->write_file = write_file;
+    con_args->mutex = &mutex;
+    con_args->client_fd = client_fd;
+    con_args->client_addr = client_addr;
+    con_args->completed = false;
+    int con_result = pthread_create(&con_thread, NULL, connection_thread_func,
+                                    (void*)con_args);
+    if (con_result) {
+      syslog(LOG_ERR, "Cannot create timer thread");
+      close(client_fd);
+      free(con_args);
+      continue;
     }
 
-    if (fseek(write_file, 0, SEEK_SET) != 0) {
-      syslog(LOG_ERR, "fseek failed");
-    }
+    th_node_t* node = malloc(sizeof(th_node_t));
+    node->data = con_args;
+    node->thread_id = con_thread;
+    SLIST_INSERT_HEAD(&head, node, next);
 
-    // send -----------------------------------------------
-    if (send_file(client_fd, write_file) == -1) {
-      safe_fclose(write_file);
-      close(socket_fd);
-      closelog();
-      exit(EXIT_FAILURE);  // NOLINT(concurrency-mt-unsafe)
+    // Remove completed connection from thread list---------
+    th_node_t* curr = NULL;
+    th_node_t* tmp = NULL;
+    SLIST_FOREACH_SAFE(curr, &head, next, tmp) {
+      if (curr->data->completed) {
+        syslog(LOG_INFO, "Terminate thread %ld", curr->thread_id);
+        pthread_join(curr->thread_id, NULL);
+        close(curr->data->client_fd);
+        syslog(LOG_INFO, "Closed connection from %s",
+               inet_ntoa(curr->data->client_addr
+                             .sin_addr));  // NOLINT(concurrency-mt-unsafe)
+        SLIST_REMOVE(&head, curr, th_node_s, next);
+        free(curr->data);
+        free(curr);
+      }
     }
-
-    close(client_fd);
-    syslog(LOG_INFO, "Closed connection from %s",
-           inet_ntoa(client_addr.sin_addr));  // NOLINT(concurrency-mt-unsafe)
   }
   syslog(LOG_INFO, "Caught signal, exiting");
 
-  safe_fclose(write_file);
+  // Close socket as soon as possible
   close(socket_fd);
+
+  while (!SLIST_EMPTY(&head)) {
+    th_node_t* node = SLIST_FIRST(&head);
+    pthread_join(node->thread_id, NULL);
+    close(node->data->client_fd);
+    SLIST_REMOVE_HEAD(&head, next);
+    free(node->data);
+    free(node);
+  }
+
+  // Wait for timer thread to finish
+  pthread_join(timer_thread, NULL);
+  free(timer_args);
+
+  safe_fclose(write_file);
   if (remove(write_path) == -1) {
     syslog(LOG_ERR, "Failed to remove %s", write_path);
-    closelog();
-    exit(EXIT_FAILURE);  // NOLINT(concurrency-mt-unsafe)
   }
   closelog();
   return 0;
